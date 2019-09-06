19694a3b758b8bb92d571c23739f31e0ff556d88
jason.hurtado <jason.hurtado@MXL32509X7.DRMAIN.local>
2019-09-06 14:12:25 -0400

    added method

--- Row2Repo3/src/com/row2/MainMethods.java
+++ Row2Repo3/src/com/row2/MainMethods.java
@@ -5,10 +5,15 @@ public class MainMethods {
 	public static void main(String[] args)
 	{
 		chris();
+		jason();
 	}
 	
 	public static void chris()
 	{
 		System.out.println("Super Juicy");
+	}
+	
+	public static void jason() {
+		System.out.println("jasonator tun tun");
 	}
 }
