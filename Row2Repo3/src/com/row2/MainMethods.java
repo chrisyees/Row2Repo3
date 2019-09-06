ec6e894b1f325027fd4d8be172391f8474a121d3
Christopher Yee <christopher.yee.s@gmail.com>
2019-09-06 14:06:36 -0400

    Adding chris method

--- Row2Repo3/src/com/row2/MainMethods.java
+++ Row2Repo3/src/com/row2/MainMethods.java
@@ -1,5 +1,14 @@
 package com.row2;
 
 public class MainMethods {
-
+	
+	public static void main(String[] args)
+	{
+		chris();
+	}
+	
+	public static void chris()
+	{
+		System.out.println("Super Juicy");
+	}
 }
